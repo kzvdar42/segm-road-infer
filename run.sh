365b144c652b86ca3fe23dff72467f44c63406e3
kzvdar42 <kzvdar42@gmail.com>
2022-09-14 12:33:17 +0300

    feat: added support for saving class mapping file.

--- run.sh
+++ run.sh
@@ -37,6 +37,11 @@ if [ ! -z "$EGO_BATCH_SIZE" ]; then
   EGO_BATCH_SIZE_ARG="--batch_size $EGO_BATCH_SIZE"
 fi
 
+CLS_MAPPING_OUT_ARG=""
+if [ ! -z "$CLS_MAPPING_OUT_PATH" ]; then
+  CLS_MAPPING_OUT_ARG="--out_cls_mapping $CLS_MAPPING_OUT_PATH"
+fi
+
 if [ -z "$MAIN_MODEL_CONFIG" ]; then
   MAIN_MODEL_CONFIG="configs/onnx-bisectv1-dynamic.yaml"
 fi
@@ -55,14 +60,18 @@ if [ $SKIP_EGO_VEHICLE == 0 ]; then
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   rm -rf $EGO_MASK_PATH
   mkdir $EGO_MASK_PATH
-  python infer.py $EGO_MODEL_CONFIG $1 $EGO_MASK_PATH --only_ego_vehicle --n_skip_frames -2 --out_format png --no_tqdm $EGO_INPUT_SHAPE_ARG $EGO_BATCH_SIZE_ARG #  --n_skip_frames -2 - take one frame per 2 seconds
+  python infer.py $EGO_MODEL_CONFIG $1 $EGO_MASK_PATH --only_ego_vehicle \
+         --n_skip_frames -2 --out_format png --no_tqdm $EGO_INPUT_SHAPE_ARG \
+         $EGO_BATCH_SIZE_ARG
+  #  --n_skip_frames -2 - take one frame per 2 seconds
   APPLY_EGO_MASK_FROM="--apply_ego_mask_from $EGO_MASK_PATH"
 fi
 # Calculate segmentation masks
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "~~~~~~~~~~~~~ Calculating segmentation masks ~~~~~~~~~~~~~"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-python infer.py $MAIN_MODEL_CONFIG $1 $2 $APPLY_EGO_MASK_FROM --no_tqdm $MAIN_INPUT_SHAPE_ARG $MAIN_BATCH_SIZE_ARG
+python infer.py $MAIN_MODEL_CONFIG $1 $2 $APPLY_EGO_MASK_FROM --no_tqdm \
+  $MAIN_INPUT_SHAPE_ARG $MAIN_BATCH_SIZE_ARG $CLS_MAPPING_OUT_ARG
 
 # Other onnx models
 # python infer.py configs/onnx-deeplabv3plus-r18d-dynamic.yaml $1 $2 --apply_ego_mask_from $EGO_MASK_PATH
