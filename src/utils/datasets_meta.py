dc3aca811e06e468b5a6d2f7f91a8ffd15f78dee
kzvdar42 <kzvdar42@gmail.com>
2023-02-28 12:14:00 +0300

    feat: added support for `cityscapes+ego` classes.

--- src/utils/datasets_meta.py
+++ src/utils/datasets_meta.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 _cityscapes_palette = np.zeros((256, 3), dtype=np.uint8)
-_cityscapes_palette[:20] = [
+_cityscapes_palette[:21] = [
     [128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
     [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
     [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
     [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100], [0, 80, 100],
-    [0, 0, 230], [119, 11, 32], [255, 255, 255]
+    [0, 0, 230], [119, 11, 32], [255, 255, 255], [0, 0, 0]
 ]
 
 _mapillary_palette = np.zeros((256, 3), dtype=np.uint8)
@@ -44,6 +44,7 @@ _ego_palette[[0, 255]] = [[0, 0, 0], [0, 0, 255]]
 
 datasets_meta = {
     'cityscapes': dict(palette=_cityscapes_palette, classes_file_path='datasets_meta/cityscapes-classes.txt'),
+    'cityscapes+ego': dict(palette=_cityscapes_palette, classes_file_path='datasets_meta/cityscapes+ego-classes.txt'),
     'mapillary': dict(palette=_mapillary_palette, classes_file_path='datasets_meta/mapillary-classes.txt'),
     'my_data': dict(palette=_my_data_palette, classes_file_path='datasets_meta/my-data-classes.txt'),
     'ego_data': dict(palette=_ego_palette, classes_file_path=['background', 'ego_vehicle']),
